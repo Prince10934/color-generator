a2d9cace77e1eadbfe20083a2df7cc0e9d43a95d
Prince <princekumar10934@gmail.com>
2021-06-16 19:42:28 +0530

    color grid added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,50 @@
-import React from "react";
-const App=()=> {
+import React, { useState } from "react";
+import Values from "values.js";
+import SingleColor from "./SingleColor";
+const App = () => {
+  const [color, setColor] = useState("#03a9f4");
+  const [error, setError] = useState(false);
+  const [colorList,setColorList] = useState([]);
+  const handleOnChange = (e) => {
+    setColor(e.target.value);
+    setError(false);
+  };
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    try {
+      const colors = new Values(color).all(10);
+      setColorList(colors);
+      console.log(colors);
+    } catch {
+      setError(true);
+    }
+  };
   return (
     <main>
-      <section className='header'>
-        <h2 className='name'>Color Generator</h2>
-        <form className='form'>
-          <input type='text' className='picker' />
-          <button className='btn' type='submit'>Submit</button>
+      <section className="header">
+        <h2 className="name">Color Generator</h2>
+        <form className="form" onSubmit={handleOnSubmit}>
+          <input
+            type="text"
+            className={`picker ${error ? "error" : null}`}
+            value={color}
+            placeholder="Enter Value Of Color"
+            onChange={handleOnChange}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
         </form>
       </section>
-      <section className='colorContainer'>
-        
+      <section className="colorContainer">
+        {
+          colorList.map((color)=>{
+            return <SingleColor key={color.rgb} color={color.rgb} weight={color.weight} />
+          })
+        }
       </section>
     </main>
   );
-}
+};
 
 export default App;
--- src/SingleColor.js
+++ src/SingleColor.js
@@ -1,10 +1,14 @@
-import React from 'react';
-import colors from 'values.js';
-const SingleColor=()=>{
-    return (
-        <>
-        </>
-    )
-}
+import React from "react";
+const SingleColor = ({ color, weight, index }) => {
+  return (
+    <article
+      className="singleColor"
+      style={{ backgroundColor: `rgb(${color[0]},${color[1]},${color[2]})` }}
+    >
+      <h3>{weight}%</h3>
+      <h4>{}</h4>
+    </article>
+  );
+};
 
 export default SingleColor;
